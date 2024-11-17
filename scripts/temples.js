57f950c70a9f5176de482c48fd0f2575a9f7d876
kaizzerr <kaizzergatan@gmail.com>
2024-11-16 20:42:38 -0700

    Picture Album

---
+++ scripts/temples.js
@@ -0,0 +1,10 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+const hamburger = document.querySelector('.hamburger');
+const burger = document.querySelector('.burger');
+const header = document.querySelector('header');
+hamburger.addEventListener('click', () => {
+    header.classList.toggle('open');
+    hamburger.classList.toggle('open');
+    burger.classList.toggle('open');
+});
