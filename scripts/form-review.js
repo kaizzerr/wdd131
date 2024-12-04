80d913928cea4605b30bf1bfcdc161257314b424
kaizzerr <kaizzergatan@gmail.com>
2024-12-04 09:47:42 -0700

    Product Review Form

---
+++ scripts/form-review.js
@@ -0,0 +1,8 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
+
+let reviewCount = parseInt(localStorage.getItem("reviewCount")) || 0;
+reviewCount++;
+localStorage.setItem("reviewCount", reviewCount);
+
+document.getElementById("reviewCount").textContent = reviewCount;
